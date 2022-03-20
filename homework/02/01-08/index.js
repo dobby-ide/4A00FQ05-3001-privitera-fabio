16ef25fad8db47bbbac9289b4bf33c4789270263
Fabio Privitera <fabio_privitera@yahoo.it>
2022-03-20 11:33:49 +0200

    ex 04 in index.js

--- homework/02/01-08/index.js
+++ homework/02/01-08/index.js
@@ -13,9 +13,12 @@ const connection = mysql.createConnection({
 connection.connect();
 
 // Wait for user's response.
-var id = readlineSync.question('May I have id? ');
+var id = readlineSync.question('id: ');
 
-connection.query(`select * from locations WHERE id=${id}`, (err, coords) => {
+// connection.query(`select * from locations WHERE id=${id}`, (err, coords) => {
+//placeholder add to escape, now ? refers strictly to id in table locations
+//so if user types "1 or true" it will
+connection.query(`select * from locations WHERE id= ?`, [id], (err, coords) => {
   if (err) {
     throw err;
   }
