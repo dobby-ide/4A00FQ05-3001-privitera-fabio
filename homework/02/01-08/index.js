bc0d29bdb14c54517a002cd537f37e87a3acf5db
Fabio Privitera <fabio_privitera@yahoo.it>
2022-03-21 11:57:17 +0200

    index.js: comment out of previous exercise(01,02,03,04), add module crudrepository.js and using it

--- homework/02/01-08/index.js
+++ homework/02/01-08/index.js
@@ -1,31 +1,41 @@
-require('dotenv').config();
-var readlineSync = require('readline-sync');
-const mysql = require('mysql');
+const connection = require('./database/crudrepository.js');
+const location = [20, 30];
+connection.connect((result) => console.log(result));
+connection.findAll((result) => console.log(result));
+connection.save(location, (result) => console.log(result));
+connection.deleteById(3, (result) =>
+  console.log('affected Rows: ' + result.affectedRows)
+);
+connection.findById(7, (result) => console.log(result));
+connection.close((err) => console.log(err));
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DB,
-  multipleStatements: true,
-});
+// var readlineSync = require('readline-sync');
+// const mysql = require('mysql');
 
-connection.connect();
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DB,
+//   multipleStatements: true,
+// });
 
-// Wait for user's response.
-var id = readlineSync.question('id: ');
+// connection.connect();
 
-// connection.query(`select * from locations WHERE id=${id}`, (err, coords) => {
-//placeholder add to escape, now ? refers strictly to id in table locations
-//so if user types "1 or true" it will
-connection.query(`select * from locations WHERE id= ?`, [id], (err, coords) => {
-  if (err) {
-    throw err;
-  }
-  coords.forEach((coord) =>
-    console.log(`${coord.longitude} ${coord.latitude}`)
-  );
-});
+// // Wait for user's response.
+// var id = readlineSync.question('id: ');
 
-// will wait if previously enqueued queriest
-connection.end();
+// // connection.query(`select * from locations WHERE id=${id}`, (err, coords) => {
+// //placeholder add to escape, now ? refers strictly to id in table locations
+// //so if user types "1 or true" it will
+// connection.query(`select * from locations WHERE id= ?`, [id], (err, coords) => {
+//   if (err) {
+//     throw err;
+//   }
+//   coords.forEach((coord) =>
+//     console.log(`${coord.longitude} ${coord.latitude}`)
+//   );
+// });
+
+// // will wait if previously enqueued queriest
+// connection.end();
