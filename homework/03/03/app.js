079443cd0278a611bac3835eec3589992401e905
Fabio Privitera <fabio_privitera@yahoo.it>
2022-03-27 13:37:34 +0300

    refactoring, id is autoincrementing, small bug fixes(id), error handling and status code

--- homework/03/03/app.js
+++ homework/03/03/app.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 
+
 const Ajv = require('ajv');
 const ajv = new Ajv();
 
@@ -12,11 +13,13 @@ const schema = {
     latitude: { type: 'integer', minimum: -90, maximum: 90 },
     longitude: { type: 'integer', minimum: -180, maximum: 180 },
   },
+  required: ['latitude', 'longitude'],
 };
 
 const validate = ajv.compile(schema);
 
 const locations = require('./routes/locations.js');
+
 let dummyData = [
   { id: 1, latitude: 26, longitude: 30 },
   { id: 2, latitude: 26, longitude: 34 },
@@ -32,42 +35,64 @@ let dummyData = [
 const app = express();
 //adding a middleware to make the string passed to work as a json
 app.use(express.json());
-app.get('/locations', (req, res) => {
-  res.send(dummyData);
-});
-app.get('/locations/:id([0-9]+)', (req, res) => {
+const getAllLocations = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: dummyData.length,
+    locations: dummyData,
+  });
+};
+const getLocation = (req, res) => {
   id = req.params.id;
   let found = dummyData.find((location) => Number(location.id) === Number(id));
   if (found) {
-    res.send(found);
+    res.status(200).json({
+      status: 'success',
+      results: 1,
+      location: found,
+    });
   } else {
-    res.send('<h1>not found</h1>');
+    res.status(404).send('<h1>not found</h1>');
   }
-});
-app.delete('/locations/:id([0-9]+)', (req, res) => {
+};
+const deleteLocation = (req, res) => {
   id = req.params.id;
   let newDb = dummyData.filter((location) => location.id != id);
 
   console.log(newDb);
   if (newDb.length != dummyData.length) {
     dummyData = [...newDb];
-    res.send({ id: id, status: 'deleted' });
+    res.status(204).json({ id: id, status: 'deleted' });
   } else {
-    res.send('<h1>Cannot delete</h1>');
+    res.status().send('<h1>Cannot delete</h1>');
   }
-});
-//so when app is receiving that post http request........ from req.body
-app.post('/locations', (req, res) => {
+};
+const postLocation = (req, res) => {
   let location = req.body;
   const valid = validate(location);
   if (valid) {
-    dummyData.push(location);
-    res.status(201).send(location);
+    let id = dummyData[dummyData.length - 1].id + 1;
+    let locationWithId = {
+      id: id,
+      latitude: location.latitude,
+      longitude: location.longitude,
+    };
+    dummyData.push(locationWithId);
+    res.status(201).json({
+      STATUS: 'SUCCESS',
+      data: locationWithId,
+    });
   } else {
     res.status(400).end();
   }
-});
-app.use('/locations', locations);
+};
+app.route("/locations").get(getAllLocations).post(postLocation);
+app.route('/locations/:id([0-9]+)').delete(deleteLocation).get(getLocation);
+
+//app.get('/locations/:id([0-9]+)', getLocation);
+//app.delete('/locations/:id([0-9]+)', deleteLocation);
+//app.post('/locations', postLocation);
+//app.use('/locations', locations);
 const server = app.listen(3000, () => {
   console.log(`Listening on port ${server.address().port}`);
 });
